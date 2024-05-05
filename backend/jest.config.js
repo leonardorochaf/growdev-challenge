ca8a11250366cffba0c61f8cdd1d142a7e09f505
Leonardo Rocha <leonardo.rocha0803@gmail.com>
2024-05-05 12:41:49 -0300

    remove files from coverage

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/server.ts',
+    '!<rootDir>/src/config/**',
+    '!<rootDir>/src/database/**',
+    '!<rootDir>/src/mocks/**',
+    '!<rootDir>/src/log/**',
+    '!<rootDir>/src/**/*.schemas.ts',
+    '!<rootDir>/src/**/*.repository.ts',
+  ],
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
